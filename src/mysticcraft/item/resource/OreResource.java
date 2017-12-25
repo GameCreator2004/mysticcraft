401e731dffdd8056b77ffb24f8481bc08da840e9
GameCreator2004 <33425135+GameCreator2004@users.noreply.github.com>
2017-12-25 21:46:02 +0700

    Add files via upload

---
+++ src/mysticcraft/item/resource/OreResource.java
@@ -0,0 +1,9 @@
+package mysticcraft.item.resource;
+
+public class OreResource extends Resource {
+
+	public OreResource(String name, int sprite, int color) {
+		super(name, sprite, color);
+	}
+
+}
